83efb3675a5d49214373e6ea2fd83101fc2d7d30
sankha-mondal <99641675+sankha-mondal@users.noreply.github.com>
2026-01-21 23:29:12 +0530

    Fix exception handling in call_fun_1

--- Python/Exception_Handeling/Exception_codes.py
+++ Python/Exception_Handeling/Exception_codes.py
@@ -124,11 +124,11 @@ def call_fun_1(num):
         print("call_fun_1")
         print(e)
 
+call_fun_1(0)
+call_fun_1(-3)
+
 # O.p:
 '''
-	call_fun_1(0)
-	call_fun_1(-3)
-	
 	call_fun_3
 	call_fun_2
 	call_fun_1
